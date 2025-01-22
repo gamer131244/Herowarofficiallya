d8173989cae18c184d625035cf8ef7d41e695c6c
MrAdeel6t6 <mrtecnical13@gmail.com>
2025-01-22 16:19:48 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log('Hello World!');
+
+
+
+document.querySelector('.content').style.height = window.innerHeight + "px";
+
+
+document.querySelector('.content').style.width = window.innerWidth + "px";
+
+console.log(window.innerWidth, window.innerHeight)
+
+
+document.querySelector('footer').style.width = window.innerWidth + 'px';
+
+
+document.querySelector('.footer-wrapper').style.marginTop = window.innerHeight - 500 + 'px';
+
+
+document.querySelector("header").style.marginTop = window.innerHeight - window.innerHeight - 10 + 'px';
